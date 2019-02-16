125265a18c18f865d4964b6debb3dc9200dc7b34
Martinez Maria Emilia <emi_51_lp@hotmail.com>
2019-02-16 20:48:58 -0300

    falta jugar razas

---
+++ app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/JugarCruza.java
@@ -0,0 +1,107 @@
+package com.trabajofinal.razasypelajescercatomartinez;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import static java.lang.Thread.sleep;
+
+public class JugarCruza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jugar_cruza);
+
+        ImageButton volver= (ImageButton)findViewById(R.id.volver);
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton cab1 = (ImageButton) findViewById(R.id.caballo1);
+
+        cab1.setOnClickListener(new View.OnClickListener() {
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.relincho1);
+
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton cab2 = (ImageButton) findViewById(R.id.caballo2);
+
+        cab2.setOnClickListener(new View.OnClickListener() {
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.resopla);
+
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                try {
+                    sleep(1000);
+                    mp.start();
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton cab3 = (ImageButton) findViewById(R.id.caballo3);
+
+        cab3.setOnClickListener(new View.OnClickListener() {
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.resopla);
+
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                try {
+                    sleep(1000);
+                    mp.start();
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton cab4 = (ImageButton) findViewById(R.id.caballo4);
+
+        cab4.setOnClickListener(new View.OnClickListener() {
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.resopla);
+
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                try {
+                    sleep(1000);
+                    mp.start();
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/MainActivity.java
+++ app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/MainActivity.java
@@ -1,7 +1,11 @@
 package com.trabajofinal.razasypelajescercatomartinez;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//jugar razas
+        Button jugarr = findViewById(R.id.jugarr);
+        jugarr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in= new Intent(getApplicationContext(),JugarRazas.class);
+                startActivity(in);            }
+        });
+//jugar cruzas
+        Button jugarc = findViewById(R.id.jugarc);
+        jugarc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getApplicationContext(),JugarCruza.class);
+                startActivity(i);            }
+        });
+//reconocimiento
+       /* ImageButton rec = findViewById(R.id.reconocimiento);
+        conf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getApplicationContext(),Configuracion.class);
+                startActivity(i);            }
+        });*/
+//Configuracion
+        ImageButton conf = findViewById(R.id.configuracion);
+        conf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getApplicationContext(),Configuracion.class);
+                startActivity(i);            }
+        });
+//about
+        ImageButton about = findViewById(R.id.about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getApplicationContext(),About.class);
+                startActivity(i);            }
+        });
+
+        //FragmentManager FM = getFragmentManager();
+        //FragmentTransaction FT = FM.beginTransaction();
+        //Fragment fragment = new FragmentUNO();
+        //FT.add(R.id.fragment_container, fragment);
+        //FT.commit();
     }
 }
