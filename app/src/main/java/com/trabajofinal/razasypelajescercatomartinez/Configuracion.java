7745c13700bd523cf08182656d76dd63a7f64441
Martinez Maria Emilia <emi_51_lp@hotmail.com>
2019-02-22 17:23:01 -0300

    configuracion

--- app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/Configuracion.java
+++ app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/Configuracion.java
@@ -1,14 +1,59 @@
 package com.trabajofinal.razasypelajescercatomartinez;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class Configuracion extends AppCompatActivity {
+    Switch levelSwitch;
+    Switch audioSwitch;
+    RadioGroup interactionRadioGroup;
+    RadioGroup recoViewModeRadioGroup, recoFilterRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+
+        setValues();
+    }
+
+    private void setValues() {
+        levelSwitch = findViewById(R.id.levelSwitch);
+        audioSwitch = findViewById(R.id.audioSwitch);
+        //recoFilterRadioGroup = findViewById(R.id.filterViewModeRadioGroup);
+        interactionRadioGroup = findViewById(R.id.interactionRadioGroup);
+        recoViewModeRadioGroup = findViewById(R.id.recoViewModeRadioGroup);
+
+        // set all values
+        SharedPreferences configPreferences = getSharedPreferences(getString(R.string.config_preferences),Context.MODE_PRIVATE);
+        levelSwitch.setChecked(configPreferences
+                .getBoolean(getString(R.string.level2_pref_key), false));
+        audioSwitch.setChecked(configPreferences
+                .getBoolean(getString(R.string.fem_audio_pref_key), false));
+     //   recoFilterRadioGroup.check(configPreferences.getInt(getString(R.string.reco_filter_key), R.id.r));
+        interactionRadioGroup.check(configPreferences
+                .getInt(getString(R.string.interaction_pref_key), R.id.InteracARadBtn));
+        recoViewModeRadioGroup.check(configPreferences
+                .getInt(getString(R.string.reco_view_mode_pref_key), R.id.listRadioBtn));
+    }
+
+    public void onAccept(View view) {
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.config_preferences),Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(getString(R.string.fem_audio_pref_key), audioSwitch.isChecked());
+        editor.putBoolean(getString(R.string.level2_pref_key), levelSwitch.isChecked());
+       // editor.putInt(getString(R.string.reco_filter_key), recoFilterRadioGroup.getCheckedRadioButtonId());
+        editor.putInt(getString(R.string.interaction_pref_key), interactionRadioGroup.getCheckedRadioButtonId());
+        editor.putInt(getString(R.string.reco_view_mode_pref_key), recoViewModeRadioGroup.getCheckedRadioButtonId());
+        editor.apply();
+        finish();
     }
 
 }
--- app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/MainActivity.java
+++ app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // testing config_preferences
+        Log.d("-----------------------", "Config Preferences -----------------------");
+        SharedPreferences configPreferences = getSharedPreferences(getString(R.string.config_preferences),Context.MODE_PRIVATE);
 
+        Boolean playinLevel2 = configPreferences.getBoolean(getString(R.string.level2_pref_key), false);
+        Log.d("!!!!NIVEL2? ", String.valueOf(playinLevel2));
+
+        Boolean femAudio = configPreferences.getBoolean(getString(R.string.fem_audio_pref_key), false);
+        Log.d("!!!!FEM-AUDIO? ", String.valueOf(femAudio));
+
+        Integer viewMode = configPreferences.getInt(getString(R.string.reco_view_mode_pref_key), R.id.listRadioBtn);
+        Log.d("!!!!GRID? ", String.valueOf(viewMode == R.id.gridRadioBtn));
     }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        initLayout();
+    }
+    private void initLayout() {
+        //findViewById(R.id.reconocimientoButton).setBackgroundResource(R.drawable.ic_reconocimiento_regular);
+        //findViewById(R.id.playButton).setBackgroundResource(R.drawable.ic_jugar_regular);
+        //findViewById(R.id.infoButton).setBackgroundResource(R.drawable.ic_info_regular);
+        findViewById(R.id.configButton).setBackgroundResource(R.drawable.ic_config_regular);
+    }
+    /** Called when the user taps Settings Button */
+    public void configuracion(View view) {
+        findViewById(R.id.configButton).setBackgroundResource(R.drawable.ic_config_click);
+        Intent intent = new Intent(this, Configuracion.class);
+        startActivity(intent);
+    }
+
 }
