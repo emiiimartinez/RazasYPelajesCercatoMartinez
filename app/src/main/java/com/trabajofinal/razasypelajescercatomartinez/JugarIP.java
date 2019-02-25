a27a8f43c54fa6e14ab9f5e4d46c00bfae08904b
pilicercato <mpilicercato@gmail.com>
2019-02-24 22:53:16 -0300

    punto y coma

---
+++ app/src/main/java/com/trabajofinal/razasypelajescercatomartinez/JugarIP.java
@@ -0,0 +1,13 @@
+package com.trabajofinal.razasypelajescercatomartinez;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JugarIP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jugar_ip);
+    }
+}
